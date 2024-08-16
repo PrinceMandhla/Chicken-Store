3698a44629808ae8edd56dc2329344c4a7bb29d4
PrinceMandhla <princemandhla@gmail.com>
2024-08-16 13:20:56 +0200

    somemore

--- my-chicken-app/src/App.js
+++ my-chicken-app/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
-import Header from './components/Header';
+
 import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
 import Products from './components/Products';
-import NavBar from './components/NavBar';
 import Branches from "./components/Branches";
-import TopBar from './components/TopBar';
+import TopBar from "./components/TopBar";
 import ProductPage from './components/ProductPage';
 import Resources from './components/Resources'
 
